144d9b5fbf2969fdb6db381d48aa85d31d795098
JoseFlores25 <74205482+JoseFlores25@users.noreply.github.com>
2022-04-24 19:09:39 -0400

    Done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        {/* <Route path="/about" element={<About />} /> */}
       </Routes>
     </BrowserRouter>
   );
